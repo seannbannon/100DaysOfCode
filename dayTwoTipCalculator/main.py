c57e6d772a8913486b8efba776006d7d7801bb75
Sean Bannon <seannbannon@gmail.com>
2024-01-20 12:57:53 -0600

    formatted to give two decimal places

--- dayTwoTipCalculator/main.py
+++ dayTwoTipCalculator/main.py
@@ -1,9 +1,9 @@
 print("Welcome to the Tip Calulator!")
-price = float(input("What was the total cost of the bill?"))
+price = float(input("What was the total cost of the bill? $"))
 tip = int(input("How much tip would you like to leave?"))
 tip_to_percent = (tip/100)
 tip_in_dollars = price * tip_to_percent
 number_of_people = int(input("How many people to split the bill?"))
 final_amount = (price + tip_in_dollars) / number_of_people
-
-print(f"Each person should pay: {final_amount}")
+rounded_final_amount = "{:.2f}".format(final_amount)
+print(f"Each person should pay: ${rounded_final_amount}")
